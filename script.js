e99c39afbfbe3551f6efce202cde95ade0905b33
alingetz <134827518+alingetz@users.noreply.github.com>
2024-02-03 14:03:16 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
-        var pos = document.documentElement;
-        pos.addEventListener('mousemove', function(e) {
-            pos.style.setProperty('--x', e.clientX + 'px');
-            pos.style.setProperty('--y', e.clientY + 'px');
-        });
-    
-        var checkbox = document.querySelector('input[type="checkbox"]');
-        var statusText = document.querySelector('.status');
-    
-        checkbox.addEventListener('change', function() {
-            if (this.checked) {
-                document.body.style.background = 'rgb(103, 89, 39)';
-                statusText.textContent = 'Lights On?';
-            } else {
-                document.body.style.background = '#333';
-                statusText.textContent = 'Lights Off?';
-            }
-        });
+    var pos = document.documentElement;
+    pos.addEventListener('mousemove', function(e) {
+        pos.style.setProperty('--x', e.clientX + 'px');
+        pos.style.setProperty('--y', e.clientY + 'px');
     });
-    
+
+    var checkbox = document.querySelector('input[type="checkbox"]');
+    var statusText = document.querySelector('.status');
+    var clouds = document.querySelector('.clouds');
+
+    checkbox.addEventListener('change', function() {
+        if (this.checked) {
+            document.body.style.background = '#333';
+            statusText.textContent = 'Lights On?';
+            clouds.classList.add('clouds-visible');
+        } else {
+            document.body.style.background = '#333';
+            statusText.textContent = 'Lights Off?';
+            clouds.classList.remove('clouds-visible');
+        }
+    });
+});
